8020763ab08953964ac2e88f168b505244c8415a
Mandeep Verma <mandeepv98@gmail.com>
2021-07-23 20:06:40 +0530

    Update JobConfiguration.java

--- Chudap/src/main/java/com/howtodoinjava/batch/decorator/config/JobConfiguration.java
+++ Chudap/src/main/java/com/howtodoinjava/batch/decorator/config/JobConfiguration.java
@@ -1,8 +1,6 @@
 package com.howtodoinjava.batch.decorator.config;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 import javax.sql.DataSource;
 
 import com.howtodoinjava.batch.decorator.processor.CustomItemProcessor;
@@ -13,28 +11,12 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.JdbcPagingItemReader;
-import org.springframework.batch.item.database.Order;
-import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
-import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.FlatFileItemWriter;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.support.ClassifierCompositeItemWriter;
-import org.springframework.batch.item.xml.StaxEventItemWriter;
+import org.springframework.batch.item.file.transform.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.oxm.xstream.XStreamMarshaller;
-import com.howtodoinjava.batch.decorator.aggregator.CustomLineAggregator;
-import com.howtodoinjava.batch.decorator.classifier.CustomerClassifier;
-import com.howtodoinjava.batch.decorator.mapper.CustomerRowMapper;
 import com.howtodoinjava.batch.decorator.model.Customer;
 
 @Configuration
@@ -59,18 +41,41 @@ public class JobConfiguration {
         return new CustomItemProcessor();
     }
 
+    private FieldExtractor<Customer> createStudentFieldExtractor() {
+        BeanWrapperFieldExtractor<Customer> extractor =
+                new BeanWrapperFieldExtractor<>();
+        extractor.setNames(new String[]{
+                "id",
+                "firstName",
+                "lastName",
+                "birthdate"
+        });
+        return extractor;
+    }
+
+    private LineAggregator<Customer> createStudentLineAggregator() {
+        DelimitedLineAggregator<Customer> lineAggregator =
+                new DelimitedLineAggregator<>();
+        lineAggregator.setDelimiter(";");
+
+        FieldExtractor<Customer> fieldExtractor = createStudentFieldExtractor();
+        lineAggregator.setFieldExtractor(fieldExtractor);
+
+        return lineAggregator;
+    }
     @Bean
     public FlatFileItemWriter<Customer> jsonItemWriter() throws Exception {
 
         String customerOutputPath = File.createTempFile("customerOutput", ".out").getAbsolutePath();
         System.out.println(">> Output Path = " + customerOutputPath);
+        LineAggregator<Customer> lineAggregator = createStudentLineAggregator();
         FlatFileItemWriter<Customer> writer = new FlatFileItemWriter<Customer>();
-        writer.setLineAggregator(new CustomLineAggregator());
+        writer.setLineAggregator(lineAggregator);
         writer.setResource(new FileSystemResource(customerOutputPath));
         writer.afterPropertiesSet();
         return writer;
     }
-    
+
     @Bean
     public Step step1() throws Exception {
         return stepBuilderFactory.get("step1")
